19d9c2f76cd6ba68334811d5ead5a7f27657ec6e
Thomas van de Meer <t.vandemeer@gmail.com>
2024-03-04 08:13:08 +0100

    Initial commit

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoriesController(IMapper mapper, ApplicationDbContext dbContext)
+        {
+            _mapper = mapper;
+            _dbContext = dbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var newCategory = _mapper.Map<Category>(request);
+
+            await _dbContext.Categories.AddAsync(newCategory);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(_mapper.Map<CategoryDto>(newCategory));
+        }
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Helpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+
+namespace CodePulse.API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CreateCategoryRequestDto, Category>();
+            CreateMap<Category, CategoryDto>();
+        }
+    }
+}
